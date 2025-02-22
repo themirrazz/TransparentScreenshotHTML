65230194e1b0eba12b22d6dced93e9d9429b9b86
themirrazz <89490659+themirrazz@users.noreply.github.com>
2025-02-21 21:19:22 -0800

    Create honorifics.js

---
+++ honorifics.js
@@ -0,0 +1,10 @@
+// LICENSE https://github.com/kyosukeweb/honorifics.js/blob/main/LICENSE.md
+// QuickAndDirty
+// Easily update any text
+setInterval(function () {
+    var query = window.__HonorificsElementContainers__ || '[data-honorific]';
+    var all = Array.from(document.querySelectorAll(query));
+    for(var i = 0; i < all.length; i++) {
+        all[i].style.hidden = navigator.honorifics ? false : true;
+    }
+}, window.__HonorificsRefreshTime__ || 100);
